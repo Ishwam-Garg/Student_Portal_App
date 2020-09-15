4827c9a3dfe805d6cad565008559197dbc1ab3a8
unknown <ishwamgarg19@gmail.com>
2020-09-15 23:48:31 +0530

    Realigned Drawer Elements

--- lib/homedrawer.dart
+++ lib/homedrawer.dart
@@ -21,20 +21,20 @@ class HomeDrawer extends StatelessWidget {
           ),
           Container(padding: EdgeInsets.only(top: 20, left: 8, right: 8, bottom: 8),
             color: Colors.white,
-            child:Row(children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100.0),
-                child: Image.asset("images/programming.gif", width: 80, height: 80, fit: BoxFit.fill,),
-              ),
-              SizedBox(width: 8,),
-              RichText(text: TextSpan(children: [
-                TextSpan(text: "Ishwam Garg\n", style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Montserrat', color: Colors.black87)),
-                TextSpan(text: "@ishwam", style: TextStyle(fontFamily: 'Montserrat', color: Colors.black54)),
-              ]),),
+            child: Column(children: <Widget>[
+              Row(children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: Image.asset("images/programming.gif", width: 80, height: 80, fit: BoxFit.fill,),
+                ),
+                SizedBox(width: 8,),
+                RichText(text: TextSpan(children: [
+                  TextSpan(text: "Ishwam Garg\n", style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Montserrat', color: Colors.black87)),
+                  TextSpan(text: "@ishwam", style: TextStyle(fontFamily: 'Montserrat', color: Colors.black54)),
+                ]),),
+              ],),
               Container(
-                alignment: Alignment.centerRight,
-                margin: EdgeInsets.only(left: 0,),
-                padding: EdgeInsets.only(top: 50,left: 0,bottom: 0),
+                alignment: Alignment.topLeft,
                 child: Column(
                   children: <Widget>[
                     ClipRRect(
@@ -55,7 +55,7 @@ class HomeDrawer extends StatelessWidget {
                 ),
               )
             ],),
-          ), //View Button
+          ), //My short Profile
           Divider(height: 1, thickness: 1, color: Colors.blueGrey[900] ),
           colleague("Anand Srivastava\n","@Anand","images/student.gif",context),
           Divider(height: 1, thickness: 1, color: Colors.blueGrey[900] ),
@@ -86,40 +86,41 @@ class HomeDrawer extends StatelessWidget {
     return Container(padding: EdgeInsets.only(top: 25, left: 8, right: 8, bottom: 25),
       color: Colors.white,
       //key: username,
-      child:Row(children: <Widget>[
-        ClipRRect(
-          borderRadius: BorderRadius.circular(100.0),
-          child: Image.asset(image, width: 80, height: 80, fit: BoxFit.fill,),
-        ),
-        SizedBox(width: 8,),
-        RichText(text: TextSpan(children: [
-          TextSpan(text: name, style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Montserrat', color: Colors.black87)),
-          TextSpan(text: username, style: TextStyle(fontFamily: 'Montserrat', color: Colors.black54)),
-        ]),),
-        Container(
-          alignment: Alignment.centerRight,
-          margin: EdgeInsets.only(left: 0,),
-          padding: EdgeInsets.only(top: 50,left: 0,bottom: 0),
-          child: Column(
-            children: <Widget>[
-              ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: RaisedButton(onPressed: (){
-                    Navigator.push(context,
+      child: Column(
+        children: <Widget>[
+          Row(children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100.0),
+              child: Image.asset(image, width: 80, height: 80, fit: BoxFit.fill,),
+            ),
+            SizedBox(width: 8,),
+            RichText(text: TextSpan(children: [
+              TextSpan(text: name, style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Montserrat', color: Colors.black87)),
+              TextSpan(text: username, style: TextStyle(fontFamily: 'Montserrat', color: Colors.black54)),
+            ]),),
+          ],),
+          Container(
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: <Widget>[
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: RaisedButton(onPressed: (){
+                      Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>Profile_gen(name,username,image)),
                       );
                     },
-                    color: Colors.blue,
-                    child: Text("View",style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      color: Colors.white,
-                    ),),
-                  )
-              ),
-            ],
-          ),
-        )
-      ],),
+                      color: Colors.blue,
+                      child: Text("View",style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                      ),),)
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
